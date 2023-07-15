d6b755a4d1c05f39cbb784d07fb26441ebdea82d
aravind192 <19h61a05j2@gmail.com>
2023-07-15 13:04:22 +0530

    Movie-Recommender

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import streamlit as st
 import requests
 
 
+
+
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
     data = requests.get(url)
